e02c4c3a9b1bcf79aaad6abbc4f494dbd6e424c1
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-28 06:22:31 +0000

    build: init tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // NAME: Astro Configuration
 // PATH: /astro.config.mjs
 
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
